2035b12781a96dafe259937672bc1b31a4d7a30a
ricardolopestomaz <ricardolptz42@gmail.com>
2025-10-25 01:01:09 -0300

    Adicionado SecurityConfig para criptrografar senha e Adicionado Service Usuario

--- src/main/java/com/petshop/api/repository/usuario/UsuarioRepository.java
+++ src/main/java/com/petshop/api/repository/usuario/UsuarioRepository.java
@@ -4,6 +4,19 @@ import com.petshop.api.model.usuario.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional; // Importe o Optional
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+
+    List<UsuarioModel> findByNomeContaining(String nome);
+
+    // boolean findByEmail(String email); // <-- REMOVA ESTA LINHA (ou corrija)
+
+    // Este é usado para verificar duplicados no cadastro
+    boolean existsByEmail(String email);
+
+    // Este é o método CORRETO que o seu 'verificarLogin' precisa
+    Optional<UsuarioModel> findByEmail(String email);
 }
